d69f0bca8ed4ad41d39598158306f5bf9c9e4c85
dckidd452 <dckidd452@gmail.com>
2017-06-12 13:27:40 +0800

    add 561

---
+++ 535-Encode_and_Decode_TinyURL/solution.py
@@ -0,0 +1,29 @@
+# Solution
+class Codec:
+
+    def encode(self, longUrl):
+        """Encodes a URL to a shortened URL.
+
+        :type longUrl: str
+        :rtype: str
+        """
+        longUrl
+
+    def decode(self, shortUrl):
+        """Decodes a shortened URL to its original URL.
+
+        :type shortUrl: str
+        :rtype: str
+        """
+
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(url))
+# Test Case
+a=Codec()
+url="www.google.com"
+codec.decode(codec.encode(url))
+
+
+
---
+++ 561-Array_Partition_I/solution.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def arrayPairSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums = sorted(nums)
+        nums_d2=nums[::2]
+        return sum(nums_d2)
